5b8bd501bb8b0c4f05b0593c771071c5ad5157d1
John-5014 <kaiserjohn52@gmail.com>
2019-10-06 18:18:01 +0300

    create user class and initializing the self method

---
+++ user.py
@@ -0,0 +1,21 @@
+class User:
+
+  user_list = []
+
+  def __init__(self, first_name,last_name, password):
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.password = password
+
+  def save_user(self):
+    User.user_list.append(self)
+    
+
+  def delete_user(self):
+
+    User.user_list.remove(self)
+
+
+class Credentials:
+      pass
