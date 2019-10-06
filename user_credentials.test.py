aa9a770e3219c0b88ff1ba92bd299abe72c75e10
John-5014 <kaiserjohn52@gmail.com>
2019-10-06 19:10:58 +0300

    instatntiate the save method to check if it passes plus checking the len of users

--- user_credentials.test.py
+++ user_credentials.test.py
@@ -35,6 +35,17 @@ class TestUser(unittest.TestCase):
     self.assertEqual(self.new_user.first_name,"Kaiser")
     self.assertEqual(self.new_user.last_name,"John")
     self.assertEqual(self.new_user.password,"abcdedfghi")
+  def test_save_user(self):
+    '''
+    test_save_user test case to test if the user object is saved into
+    the user list
+    '''
+
+    self.new_user.save_user()
+
+    # save the new user
+    self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
     unittest.main()
